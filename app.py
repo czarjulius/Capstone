be50d72a687b56674c02e2d957107cf474ea1288
czarjulius <juliusczar.jc@gmail.com>
2019-12-21 12:05:37 +0100

    added folder structures

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask import Flask, jsonify, abort, request
+from models import setup_db, db, Movie, Actor
+from auth.auth import AuthError, requires_auth
+from validate import *
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
+
+@app.route('/')
+def welcome():
+  return 'Welcome to Capstone Hub!'
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
